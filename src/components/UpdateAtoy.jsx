403e6667b6473b8e4bf8eb7be49662cbc96f33c0
Shishir758 <studentshishir@gmail.com>
2023-05-20 01:16:35 +0600

    Update toy Page Added

---
+++ src/components/UpdateAtoy.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import Footer from './Footer';
+import Header from './Header';
+
+const UpdateAtoy = () => {
+  const [singleToy, setSingleToy] = useState([]);
+  const toys = useLoaderData();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const toy = toys.find(toy => toy._id == id);
+    setSingleToy(toy);
+  }, [singleToy, id]);
+
+const handleUpdate =(event)=>{
+  event.preventDefault();
+  const form = event.target;
+  const price = form.price.value;
+  const quantity = form.quantity.value;
+  const description = form.description.value;
+  const updateInfo ={price, quantity, description};
+  console.log(updateInfo);
+
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, update it!'
+  })
+  .then((result) => {
+    if (result.isConfirmed) {
+      fetch(`http://localhost:5000/products/${id}`,{
+        method: 'PUT',
+        headers:{'content-type':'application/json'},
+        body:JSON.stringify(updateInfo)
+      })
+      .then(res => res.json())
+      .then(data =>{
+        if (data.modifiedCount > 0) {
+          Swal.fire(
+            'Update!',
+            'Information has been updated.',
+            'success'
+          );
+          window.location.href = '/mytoys';
+        }
+      })
+     
+    }
+  })
+
+
+}
+
+  return (
+    <>
+    <Header></Header>
+      <div className='md:mx-64 lg:mx-48'>
+        <h2 className="text-2xl font-bold mb-4 mt-8 text-center">Update Toy's Information</h2>
+        <form onSubmit={handleUpdate} id="toyForm" className="space-y-4">
+
+          <div className="flex flex-col">
+            <label htmlFor="pictureUrl" className="text-lg font-semibold mb-1">
+            Price:
+            </label>
+            <input className="border border-gray-300 rounded-md py-2 px-3"
+              type="text" id="pictureUrl"
+              name="price" defaultValue={singleToy.price} />
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="pictureUrl" className="text-lg font-semibold mb-1">
+            Quantity:
+            </label>
+            <input className="border border-gray-300 rounded-md py-2 px-3"
+              type="text" id="pictureUrl"
+              name="quantity" defaultValue={singleToy.quantity}/>
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="pictureUrl" className="text-lg font-semibold mb-1">
+            Description:
+            </label>
+            <input className="border border-gray-300 rounded-md py-2 px-3"
+              type="text" id="pictureUrl"
+              name="description" defaultValue={singleToy.description}/>
+          </div>
+
+
+
+
+          <div className='text-center mb-8'><button
+            type="submit"
+            className=" mb-4 bg-blue-500 hover:bg-blue-600 text-white rounded-md py-2 px-4"
+          >
+            Update Toy Information
+          </button>
+          
+          </div>
+        </form>
+      </div>
+<Footer></Footer>
+    </>
+  );
+};
+
+export default UpdateAtoy;
