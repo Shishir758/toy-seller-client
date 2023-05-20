a7abe46c5c5f2626b79ee1773083b72c9df6ca00
Shishir758 <studentshishir@gmail.com>
2023-05-20 22:14:29 +0600

    Blog Page mobile responsive

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -13,14 +13,14 @@ const blog = () => {
       <h4 className="mb-5 text-center mt-6 font-bold">
         FAQ of Assignment Eleven(11)
       </h4>
-      <div className="grid  items-center mx-20 text-justify">
+      <div className="grid  items-center md:mx-20 lg: lg:mx-20 mx-6 text-justify">
 
         <div className="mt-5 ">
 
 
         <p><b>1. What is an access token and refresh token?</b></p>
-          <div className='flex justify-between gap-8'>
-            <div className='w-1/2'>
+          <div className=' justify-between gap-8 md:flex lg:flex flex-row '>
+            <div className='md:w-1/2 lg:md:w-1/2'>
               
               <p><b>* Access Token:</b><br></br></p>
               <p>
@@ -28,16 +28,16 @@ const blog = () => {
             </div>
 
 
-            <div className='w-1/2'>
+            <div className='md:w-1/2 lg:md:w-1/2 mt-4'>
               <p><b>* Refresh Token:</b><br></br></p>
               <p>A refresh token is a credential that is also issued by the authentication server, along with the access token. Unlike the access token, the refresh token is long-lived and is used to obtain a new access token when the current one expires. It provides a way to obtain fresh access tokens without requiring the user to re-authenticate.</p></div>
           </div>
           <br></br>
 
           <p><b>=> How do access token and refresh token work?</b></p>
-          <div className='flex justify-between gap-8'>
+          <div className=' justify-between gap-8 md:flex lg:flex flex-row '>
             
-            <div> <p><b>* Access Token:</b><br></br></p>
+            <div className='md:w-1/2 lg:md:w-1/2'> <p><b>* Access Token:</b><br></br></p>
 
               1. The client authenticates with the authentication server by providing valid credentials.<br></br>
               <br></br>
@@ -60,7 +60,7 @@ const blog = () => {
 
               7. If the access token is invalid, expired, or lacks sufficient privileges, the server denies access and returns an appropriate response (e.g., 401 Unauthorized).<br></br>
               <br></br></div>
-            <div>  <p><b>* Refresh Token:</b><br></br></p>
+            <div className='md:w-1/2 lg:md:w-1/2'>  <p><b>* Refresh Token:</b><br></br></p>
 
 
               1. After the initial authentication, along with the access token, the server also issues a refresh token to the client.
@@ -90,15 +90,15 @@ const blog = () => {
             </div>
           </div>
 
-          <div><p><b>-> where should we store access token and refresh token on the client-side??</b></p>
+          <div><p><b>=> where should we store access token and refresh token on the client-side??</b></p>
             <p>You can store the access token and refresh token in the server-side session. The application can use web sessions to communicate with the server. The token is then available for any requests originating from server-side code. This is also known as the backend for frontend (BFF) proxy.
             </p></div>
             <br></br>
 
-            <p><b>=> Compare SQL and NoSQL databases?</b></p>
-          <div className='flex justify-between gap-8'>
+            <p><b>2. Compare SQL and NoSQL databases?</b></p>
+          <div className=' justify-between gap-8 md:flex lg:flex flex-row '>
             
-            <div className='w-1/2'> <p><b>* SQL Databases:</b><br></br></p>
+            <div className='md:w-1/2 lg:md:w-1/2'> <p><b>* SQL Databases:</b><br></br></p>
 
               1. Structure: SQL databases are based on a structured data model, where data is organized into tables with predefined schemas. Each table consists of rows and columns, and the relationships between tables are defined by foreign keys.<br></br>
               <br></br>
@@ -117,7 +117,7 @@ const blog = () => {
 
              <br></br>
               <br></br></div>
-            <div className='w-1/2'>  <p><b>* NoSQL Databases:</b><br></br></p>
+            <div className='md:w-1/2 lg:md:w-1/2'>  <p><b>* NoSQL Databases:</b><br></br></p>
 
 
               1. Structure: NoSQL databases are based on a flexible and schema-less data model. Data is stored in various formats like key-value pairs, documents, column families, or graphs, depending on the NoSQL database type.
@@ -139,32 +139,34 @@ const blog = () => {
             </div>
           </div>
 
-          <p><b>1. What is Express JS and Nest JS ?</b></p>
-          <div className='flex justify-between gap-8'>
-            <div className='w-1/2'>
+          <p><b>3. What is Express JS and Nest JS ?</b></p>
+          <div className=' justify-between gap-8 md:flex lg:flex flex-row '>
+            <div className='md:w-1/2 lg:md:w-1/2'>
               
               <p><b>* Express JS:</b><br></br></p>
               <p>
               Express.js, or simply Express, is a back end web application framework for building RESTful APIs with Node.js, released as free and open-source software under the MIT License. It is designed for building web applications and APIs. It has been called the de facto standard server framework for Node.js.</p>
             </div>
-            <div className='w-1/2'>
+            <br></br>
+            <div className='md:w-1/2 lg:md:w-1/2'>
+              
               <p><b>* Nest JS:</b><br></br></p>
               <p>Nest. js is one of the fastest-growing Node. js frameworks for building efficient, scalable, and enterprise-grade backend applications using Node. js. It is known for producing highly testable, maintainable, and scalable applications using modern JavaScript and TypeScript.</p></div>
           </div>
           <br></br>
           <br></br>
 
-          <p><b>1. What is MongoDB aggregate and how does it work?</b></p>
-          <div className='flex justify-between gap-8'>
-            <div className='w-1/2'>
+          <p><b>4. What is MongoDB aggregate and how does it work?</b></p>
+          <div className=' justify-between gap-8 md:flex lg:flex flex-row '>
+            <div className='md:w-1/2 lg:md:w-1/2'>
               
               <p><b>* MongoDB aggregate:</b><br></br></p>
               <p>
               MongoDB's aggregation framework is a feature that allows you to perform advanced data processing and analysis on MongoDB collections. It provides a way to perform complex queries, transformations, and calculations on the data within the database. The aggregation pipeline, a key component of the aggregation framework, enables you to apply a series of stages to the data to filter, group, sort, project, and aggregate results. It offers a flexible and efficient approach to retrieve and manipulate data in MongoDB.</p>
             </div>
 
-
-            <div className='w-1/2'>
+            <br></br>
+            <div className='md:w-1/2 lg:md:w-1/2'>
               <p><b>* How MongoDB aggregation works:</b><br></br></p>
               <p>the MongoDB aggregation pipeline allows you to process and analyze data in MongoDB collections using a sequence of stages. Each stage performs specific operations on the data and passes the results to the next stage. The pipeline can include stages for matching, filtering, grouping, sorting, transforming, and more. It provides a powerful way to perform complex data manipulations and calculations in MongoDB.</p></div>
           </div>
